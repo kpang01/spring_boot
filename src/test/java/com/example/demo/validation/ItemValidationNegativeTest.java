4a4d13be2443ea2d064a30ded84139043d9b4769
kpang01 <pkhongjun.gmail.com>
2025-07-22 14:36:43 +0800

    Day 2

---
+++ src/test/java/com/example/demo/validation/ItemValidationNegativeTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.validation;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class ItemValidationNegativeTest {
+
+	//TODO: SpringBoot:Practical 3 - Testing: Introduction to unit testing and integration testing with Spring Boot.
+	//Complete the code below then commit your code
+	//Refer to ItemValidationPositiveTest code as reference
+
+	@Test
+	
+    @DisplayName("parseAndValidateLongId: Should throw IllegalArgumentException for a non-numeric string")
+    void parseAndValidateLongId_InvalidCase_NonNumeric() {
+    	// complete the code here
+		String idString = "nsodvniowebvew";
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+            ItemValidation.parseAndValidateLongId(idString);
+        });
+        assertEquals("Invalid ID format. ID must be a valid number.", thrown.getMessage());
+    }
+}
